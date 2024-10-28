bb0a8af18b857c13c4a4db629f975393c9a6f3dd
haikaljs <haikaljavascript@gmail.com>
2024-10-29 04:57:51 +0800

    Find max number - Basic challenge1

---
+++ 01-basic-challenges-1/05-find-max-number/max-number.js
@@ -0,0 +1,20 @@
+function maxNumber(arr) {
+  // Solution 1
+  // return Math.max(...arr)
+
+  // Solution 2
+
+  // set max to first index
+  let max = arr[0];
+  // loop each element start with second index
+  for (let i = 1; i < arr.length; i++) {
+    // compare each element with max
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(maxNumber([1, 2, 3, 4, 5, 6, 7, 8, 100, 800]));
